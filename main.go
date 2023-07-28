d0e08f7d4f6abc48ca18abb67e900bf34b9538b0
leekool <lee@imre.al>
2023-07-28 23:18:14 +1000

    file read

--- main.go
+++ main.go
@@ -1,9 +1,20 @@
 package main
 
 import (
+	// "bufio"
 	"fmt"
+	// "io"
+	"os"
 )
 
-func main()  {
-	fmt.Println("this is a test");
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func main() {
+	dat, err := os.ReadFile("/home/lee/sync/org/todo.org")
+	check(err)
+	fmt.Print(string(dat))
 }
